1e5b06472615f9a7ae9ed9ebd976706698bbc0e5
muneebul <muneebulgulzar@gmail.com>
2022-01-20 16:36:27 +0530

    bug fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">IEvince app - version-1.0!!</h1> \n');
+  res.send('<h1 style="color:green;">Muneeb IEvince app - version-1.0!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
