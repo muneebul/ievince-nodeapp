95e87174fee78fa4b23a49688479c3625f6c17c0
muneeb <muneebulgulzar@gmail.com>
2022-01-20 17:39:34 +0530

    vv

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  
-  res.send('<h1 style="color:green;">Muneeb ss IEvince app - version-1.0!!</h1> \n');
+
+  res.send('<h1 style="color:green;">Muneeb ssaaaaaaaa IEvince app - version-1.0!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
